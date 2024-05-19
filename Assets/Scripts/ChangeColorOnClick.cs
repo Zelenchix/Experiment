9d2c3c0b62954971585cec0437502f1b0b3d1a38
Zelenchix <lizazel07@mail.ru>
2024-05-19 15:42:34 +0500

    если кликать по кубу, то меняются цвета

--- Assets/Scripts/ChangeColorOnClick.cs
+++ Assets/Scripts/ChangeColorOnClick.cs
@@ -6,8 +6,21 @@ using Random = System.Random;
 
 public class ChangeColorOnClick : MonoBehaviour
 {
+   private Renderer rn;
+   void Start()
+   {
+      rn = GetComponent<Renderer>();
+   }
    private void OnMouseDown()
    {
-      GetComponent<Renderer>().material.color = Color.yellow;
+      Color randomColor = new Color(UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f), UnityEngine.Random.Range(0f, 1f));
+      
+      rn.material.color = randomColor;
    }
+
+   private void OnClick()
+   {
+      
+   }
+   
 }
